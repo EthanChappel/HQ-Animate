5301c6378562c83ed46d43f7163f00cbfb789b56
Ethan Chappel <EthanChappel@users.noreply.github.com>
2026-01-09 04:24:22 +0000

    Build Flatpak in temporary directory.

--- flatpak-build.sh
+++ flatpak-build.sh
@@ -1,4 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
 set -e
-flatpak-builder --repo ./flatpak-repo --force-clean --install-deps-from flathub ./flatpak-build-dir com.chappelastro.HQAnimate.json
-flatpak build-bundle ./flatpak-repo HQAnimate.flatpak com.chappelastro.HQAnimate
+
+TMP_DIR=$(mktemp -d -t hqa-flatpak-build-XXXXXXXXXX)
+
+function cleanup {
+  rm -rf "$TMP_DIR"
+}
+
+trap cleanup EXIT
+
+flatpak-builder --repo $TMP_DIR/flatpak-repo --force-clean --install-deps-from flathub $TMP_DIR/flatpak-build-dir --state-dir $TMP_DIR/flatpak-builder ./com.chappelastro.HQAnimate.json
+flatpak build-bundle $TMP_DIR/flatpak-repo ./HQAnimate.flatpak com.chappelastro.HQAnimate
